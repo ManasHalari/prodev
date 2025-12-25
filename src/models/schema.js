942dccd33107ac261e201c23bed591c7474a137f
ManasHalari <manashalari@gmail.com>
2025-12-25 14:37:54 +0530

    Added dummy data

---
+++ src/models/schema.js
@@ -0,0 +1,14 @@
+export const initialColumns = [
+  { id: "col_1", name: "Name", type: "text" },
+  { id: "col_2", name: "Email", type: "email" }
+];
+
+export const initialRows = [
+  {
+    id: "row_1",
+    cells: {
+      col_1: "Alice",
+      col_2: "alice@mail.com"
+    }
+  }
+];
